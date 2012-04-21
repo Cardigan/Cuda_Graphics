462f1d3ef342114f6e5ce883c7de3896a3c04792
Jan Lorenz Soliman <jsoliman@unix2.csc.calpoly.edu>
2012-04-21 16:41:10 -0700

    Adding color and and window transforms.

--- RasterizerFunctions.cpp
+++ RasterizerFunctions.cpp
@@ -5,13 +5,44 @@ Vector3 normalize(Vector3 v) {
   return Vector3(v.x/mag, v.y/mag, v.z/mag);
 }
 
-void ColorVertices(vector<Tri *> Triangles, vector<Vector3 *> Vertices) {
+double clamp(double num, double floor, double ceiling) {
+  if (num < floor) { return floor; }
+  if (num > ceiling) { return ceiling; }
+  return num;
+}
+
+double dot(Vector3 v1, Vector3 v2) {
+  return (v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
+}
+
+
+double p2wy(double y, int height) {
+  return (((height - 1.0) / 2) * y) + ((height-1.0)/2);
+}
+
+double p2wx(double x, int width) {
+  return (((width - 1.0) / 2) * x) + ((width-1.0)/2);
+}
+
+void Convert_to_Window(vector<Vector3 *> & Vertices, int width, int height) {
+
+  for (int i = 0; i < Vertices.size(); i++) {
+    (*(Vertices[i])).x = p2wy( (*(Vertices[i])).x, width);
+    (*(Vertices[i])).y = p2wy( (*(Vertices[i])).y, height);
+  }
+
+}
+
+void ColorVertices(vector<Tri *> & Triangles, vector<Vector3 *> & Vertices) {
+
+  Vector3 light_pos = Vector3(1.0, 1.0, 1.0);
+  float material = 0.2;
+  Color color = Color(0.4, 0.1, 0.1);
 
   for (int i = 0; i < Triangles.size(); i++) {
     int i1 = (*(Triangles[i])).v1;
     int i2 = (*(Triangles[i])).v2;
     int i3 = (*(Triangles[i])).v3;
-
 
     // Calculate and save the normal
     Vector3 v1 = Vector3((*(Vertices[i1])).x - (*(Vertices[i2])).x,
@@ -29,10 +60,58 @@ void ColorVertices(vector<Tri *> Triangles, vector<Vector3 *> Vertices) {
   }
 
   for (int i = 0; i < Triangles.size(); i++) {
-    // Get color for vector 1
-    Color x = Color();
+    int i1 = (*(Triangles[i])).v1;
+    Tri tri = (*(Triangles[i]));
+    Vector3 vertex = (*(Vertices[i1]));
 
-    //Vector3 to_light = normalize(Vector3())
+    Vector3 to_light = normalize(Vector3(
+                                  light_pos.x - vertex.x,
+                                  light_pos.y- vertex.y,
+                                  light_pos.z - vertex.z ));
+
+
+    // Get color for vector 1
+    Color color = Color();
+    color.r = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.r;
+    color.g = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.g;
+    color.b = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.b;
+    (*(Triangles[i])).c1 = color;
+
+    int i2 = (*(Triangles[i])).v2;
+    tri = (*(Triangles[i]));
+    vertex = (*(Vertices[i2]));
+
+    to_light = normalize(Vector3(
+                                  light_pos.x - vertex.x,
+                                  light_pos.y- vertex.y,
+                                  light_pos.z - vertex.z ));
+
+
+    // Get color for vector 2
+    color = Color();
+    color.r = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.r;
+    color.g = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.g;
+    color.b = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.b;
+    (*(Triangles[i])).c2 = color;    
+    
+    
+    int i3 = (*(Triangles[i])).v3;
+    tri = (*(Triangles[i]));
+    vertex = (*(Vertices[i3]));
+
+    to_light = normalize(Vector3(
+                                  light_pos.x - vertex.x,
+                                  light_pos.y- vertex.y,
+                                  light_pos.z - vertex.z ));
+
+
+    // Get color for vector 2
+    color = Color();
+    color.r = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.r;
+    color.g = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.g;
+    color.b = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * color.b;
+    (*(Triangles[i])).c3 = color;   
+
   }
 
 }
