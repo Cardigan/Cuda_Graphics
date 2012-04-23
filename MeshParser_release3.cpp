8e6a8f0258958e7e70fa40e39eb7598c23692b3b
Jan Lorenz Soliman <JanJan@Jan-Lorenz-Solimans-MacBook.local>
2012-04-23 10:31:13 -0700

    Done with rasterizing.

--- MeshParser_release3.cpp
+++ MeshParser_release3.cpp
@@ -1,52 +1,7 @@
 //basic program to read in a mesh file (of .m format from H. Hoppe)
 //Hh code modified by ZJW for csc 471
+#include "Rasterizer.h"
 
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <vector>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <cstdio>
-#include <GL/glut.h>
-#include <assert.h>
-#include <map>
-
-using namespace std;
-
-#define FLT_MIN 1.1754E-38F
-#define FLT_MAX 1.1754E+38F
-
-//very simple data structure to store 3d points
-typedef struct Vector3
-{
-  float x;
-  float y;
-  float z;
-  Vector3(float in_x, float in_y, float in_z) : x(in_x), y(in_y), z(in_z) {}
-  Vector3() {}
-} Vector3;
-
-typedef struct Color
-{
-  float r;
-  float g;
-  float b;
-} Color;
-
-//data structure to store triangle - 
-//note that v1, v2, and v3 are indexes into the vertex array
-typedef struct Tri{
-  int v1;
-  int v2;
-  int v3;
-  Vector3 normal;
-  Color c1, c2, c3;
-  Tri(int in_v1, int in_v2, int in_v3) : v1(in_v1), v2(in_v2), v3(in_v3), normal(0, 1, 0){}
-  Tri() : normal(0, 1, 0) {}
-} Tri;
 
 //stl vector to store all the triangles in the mesh
 vector<Tri *> Triangles;
@@ -70,6 +25,272 @@ void readStream(istream& is);
 void drawTri(Tri * t);
 void drawObjects();
 void display();
+
+Vector3 normalize(Vector3 v) {
+  double mag = sqrt(v.x*v.x + v.y*v.y + v.z*v.z); 
+  if (mag > 0) {
+   return Vector3(v.x/mag, v.y/mag, v.z/mag);
+  }
+  else {
+   return Vector3(0,0,0);
+  }
+}
+
+double clamp(double num, double floor, double ceiling) {
+  if (num < floor) { return floor; }
+  if (num > ceiling) { return ceiling; }
+  return num;
+}
+
+double dot(Vector3 v1, Vector3 v2) {
+  return (v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
+}
+
+double window_size = 1.0;
+
+double p2wy(double y, int height) {
+  return (((height - window_size) / 2) * y) + ((height-window_size)/2);
+}
+
+double p2wx(double x, int width) {
+  return (((width - window_size) / 2) * x) + ((width-window_size)/2);
+}
+
+void Convert_to_Window(vector<Vector3 *> & Vertices, int width, int height) {
+
+  for (int i = 0; i < Vertices.size(); i++) {
+    (*(Vertices[i])).x = p2wx( (*(Vertices[i])).x*10, width) + 60;
+    (*(Vertices[i])).y = p2wy( ((*(Vertices[i])).y*10), height) - 200;
+  }
+
+}
+
+// Returns bounding 
+int min_val(double x1, double x2, double x3) {
+   double lowest = x1;
+   if (x2 < lowest) {
+      lowest = x2;
+   }
+   if (x3 < lowest) {
+      lowest = x3;
+   }
+   return (int)lowest;
+}
+
+int max_val(double x1, double x2, double x3) {
+   double highest = x1;
+   if (x2 > highest) { highest = x2; }
+   if (x3 > highest) { highest = x3; }
+   return (int)highest+1;
+}
+
+int in_triangle(double alpha, double gamma, double beta) {
+   return (alpha > -0.0) && (alpha < 1.01) && (gamma > -0.00) && (gamma < 1.01) && (beta > -0.0) && (beta < 1.01);
+}
+
+void RasterizeTriangles(vector<Tri *> & Triangles, vector<Vector3 *> & Vertices, int width, int height) {
+
+   color_t* img_buffer = (color_t*)malloc(sizeof(color_t)*width*height);
+   double* depth_buffer = (double*)malloc(sizeof(double)*width*height);
+
+   // Initialize depth_buffer to low number;
+   for (int i = 0; i < width*height; i++) {
+      depth_buffer[i] = -100000;
+   }
+
+   Image img(width, height);
+
+   //Iterate through each Triangle
+   for (int i = 0; i < Triangles.size(); i++) {
+
+      int t = Vertices[Triangles[i]->v1]->x;
+
+      if (Triangles[i]->normal.z < 0) {
+
+      }
+      else {
+         // Get minimum and maximum x
+         int minx = min_val(Vertices[Triangles[i]->v1]->x, Vertices[Triangles[i]->v2]->x, Vertices[Triangles[i]->v3]->x);
+         if (minx < 0) { minx = 0; }
+         int maxx = max_val(Vertices[Triangles[i]->v1]->x, Vertices[Triangles[i]->v2]->x, Vertices[Triangles[i]->v3]->x);
+         if (maxx + 1 > width ) { maxx = width-1; }
+
+         // Get minimum and maximum y
+         int miny = min_val(Vertices[Triangles[i]->v1]->y, Vertices[Triangles[i]->v2]->y, Vertices[Triangles[i]->v3]->y);
+         if (miny < 0) { miny = 0; }
+         int maxy = max_val(Vertices[Triangles[i]->v1]->y, Vertices[Triangles[i]->v2]->y, Vertices[Triangles[i]->v3]->y);
+         if (maxy + 1 > width) { maxy = width-1; }
+
+         //printf("Minx is %d. Maxx is %d\n", minx, maxx);
+
+
+         for (int y = miny; y < maxy+1; y++) {
+            for (int x = minx; x < maxx+1; x++) {
+               // Calculate alpha, beta, gamma, 
+               
+               double A = (Vertices[Triangles[i]->v2]->x - Vertices[Triangles[i]->v1]->x) * (Vertices[Triangles[i]->v3]->y - Vertices[Triangles[i]->v1]->y)  ;
+               A -= (Vertices[Triangles[i]->v3]->x - Vertices[Triangles[i]->v1]->x) * (Vertices[Triangles[i]->v2]->y - Vertices[Triangles[i]->v1]->y)   ;
+
+               double beta =  (Vertices[Triangles[i]->v1]->x - Vertices[Triangles[i]->v3]->x) * (y - Vertices[Triangles[i]->v3]->y)  ;
+               beta -=  (x - Vertices[Triangles[i]->v3]->x) * (Vertices[Triangles[i]->v1]->y - Vertices[Triangles[i]->v3]->y)  ;
+               beta /= A;
+
+               double gamma =  (Vertices[Triangles[i]->v2]->x - Vertices[Triangles[i]->v1]->x) * (y - Vertices[Triangles[i]->v1]->y)  ;
+               gamma -= (x - Vertices[Triangles[i]->v1]->x) * (Vertices[Triangles[i]->v2]->y - Vertices[Triangles[i]->v1]->y)  ;
+               gamma /= A;  
+
+               double alpha = 1.0 - beta - gamma;
+
+               // Is it inside?
+               if (in_triangle(alpha, gamma, beta)) {
+                  // Color
+                  color_t col;
+                  col.r = alpha * (Triangles[i]->c1.r) + beta * ( Triangles[i]->c2.r ) + gamma * ( Triangles[i]->c3.r  );
+                  col.g = alpha * (Triangles[i]->c1.g) + beta * ( Triangles[i]->c2.g ) + gamma * ( Triangles[i]->c3.g  );
+                  col.b = alpha * (Triangles[i]->c1.b) + beta * ( Triangles[i]->c2.b ) + gamma * ( Triangles[i]->c3.b  );
+                  //col.r = 1.0; col.g = 0.0; col.b = 0.0;
+                  //col.r = Triangles[i]->normal.x;
+                  //col.g = Triangles[i]->normal.y;
+                  //col.b = Triangles[i]->normal.z;
+
+                  // Is the depth higher than the current depth?
+                     // Calculate interpolated z
+                     float depth = alpha * (Vertices[Triangles[i]->v1]->z) + beta * ( Vertices[Triangles[i]->v2]->z) + gamma * (  Vertices[Triangles[i]->v3]->z );
+                     if (depth > depth_buffer[x*width+y]) {
+                        depth_buffer[x*width+y] = depth;
+                        //printf("Coloring %d, %d.\n", x, y);
+                        //img.pixel(x,y, col);
+                        img_buffer[x*width+y] = col;
+                     }
+               }
+            }
+         }
+
+
+      }
+
+   }
+
+   for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+         img.pixel(x,y, img_buffer[x*width+y]);
+      }
+   }
+
+   img.WriteTga("awesome.tga", true);
+   free(img_buffer);
+   free(depth_buffer);
+}
+
+void ColorVertices1(vector<Tri *> & Triangles, vector<Vector3 *> & Vertices) {
+
+  Vector3 light_pos = Vector3(0.5, 0.5, 0.5);
+  double material = 0.8;
+  Color light = Color(0.8, 0.8, 0.8);
+
+  int size = Triangles.size();
+  for (int i = 0; i < size; i++) {
+    // Change indexing from 0 start to 1
+    Triangles[i]->v1 = Triangles[i]->v1-1;
+    Triangles[i]->v2 = Triangles[i]->v2-1;
+    Triangles[i]->v3 = Triangles[i]->v3-1;
+
+
+    int i1 = (*(Triangles[i])).v1;
+    int i2 = (*(Triangles[i])).v2;
+    int i3 = (*(Triangles[i])).v3;
+
+    // Calculate and save the normal
+
+    Vector3 v1 = Vector3(Vertices[i1]->x - Vertices[i2]->x,
+                           Vertices[i1]->y - Vertices[i2]->y,
+                           Vertices[i1]->z - Vertices[i2]->z);
+
+    Vector3 v2 = Vector3( Vertices[i1]->x - Vertices[i3]->x,
+                           Vertices[i1]->y - Vertices[i3]->y,
+                           Vertices[i1]->z - Vertices[i3]->z
+                           );
+
+    /*Vector3 v2 = Vector3((*(Vertices[i1])).x - (*(Vertices[i3])).x,
+                         (*(Vertices[i1])).y - (*(Vertices[i3])).y,
+                          (*(Vertices[i1])).z - (*(Vertices[i3])).z);*/
+
+    Vector3 cross = Vector3();
+    cross.x = v1.y*v2.z - v1.z*v2.y;
+    cross.y = v1.z*v2.x - v1.x*v2.z;
+    cross.z = v1.x*v2.y - v1.y*v2.x;
+    if (cross.x < 0.0) { cross.x = 0.0; }
+    if (cross.y < 0.0) { cross.y = 0.0; }
+    Triangles[i]->normal = normalize(cross); 
+
+  }
+
+  printf("Done calculating normals.\n");
+  for (int i = 0; i < Triangles.size(); i++) {
+    int i1 = (*(Triangles[i])).v1;
+    Tri tri = (*(Triangles[i]));
+    Vector3 vertex = (*(Vertices[i1]));
+
+    Vector3 to_light = normalize(Vector3(
+                                  light_pos.x - vertex.x,
+                                  light_pos.y- vertex.y,
+                                  light_pos.z - vertex.z ));
+
+
+    // Get color for vector 1
+    Color color = Color();
+    color.r = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.r;
+    //color.r = Triangles[i]->normal.x;
+    color.g = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.g;
+    //color.g = Triangles[i]->normal.y;
+    color.b = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.b;
+    //color.b = Triangles[i]->normal.z;
+    (*(Triangles[i])).c1 = color;
+
+    int i2 = (*(Triangles[i])).v2;
+    tri = (*(Triangles[i]));
+    vertex = (*(Vertices[i2]));
+
+    to_light = normalize(Vector3(
+                                  light_pos.x - vertex.x,
+                                  light_pos.y- vertex.y,
+                                  light_pos.z - vertex.z ));
+
+
+    // Get color for vector 1
+    color = Color();
+    color.r = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.r;
+    //color.r = Triangles[i]->normal.x;
+    color.g = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.g;
+    //color.g = Triangles[i]->normal.y;
+    color.b = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.b;
+    //color.b = Triangles[i]->normal.z;
+    (*(Triangles[i])).c2 = color;
+    
+    
+    int i3 = (*(Triangles[i])).v3;
+    tri = (*(Triangles[i]));
+    vertex = (*(Vertices[i3]));
+
+    to_light = normalize(Vector3(
+                                  light_pos.x - vertex.x,
+                                  light_pos.y- vertex.y,
+                                  light_pos.z - vertex.z ));
+
+
+    // Get color for vector 1
+    color = Color();
+    color.r = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.r;
+    //color.r = Triangles[i]->normal.x;
+    color.g = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.g;
+    //color.g = Triangles[i]->normal.y;
+    color.b = material * (clamp((dot(tri.normal, to_light)), 0.0, 1.0)) * light.b;
+    //color.b = Triangles[i]->normal.z;
+    (*(Triangles[i])).c3 = color; 
+
+  }
+
+}
 
 //open the file for reading
 void ReadFile(char* filename) {
@@ -183,13 +404,27 @@ void readLine(char* str) {
 void printFirstThree() {
   printf("first vertex: %f %f %f \n", Vertices[0]->x, Vertices[0]->y, Vertices[0]->z);
   printf("first face: %d %d %d \n", Triangles[0]->v1, Triangles[0]->v2, Triangles[0]->v3);    
-  printf("second vertex: %f %f %f \n", Vertices[1]->x, Vertices[1]->y, Vertices[1]);
-  printf("second face: %d %d %d \n", Triangles[1]->v1, Triangles[1]->v2, Triangles[1]->v3);
+  printf("second vertex: %f %f %f \n", Vertices[1]->x, Vertices[1]->y, Vertices[1]->z);
+  //printf("second face: %d %d %d \n", Triangles[1]->v1, Triangles[1]->v2, Triangles[1]->v3);
   printf("third vertex: %f %f %f \n", Vertices[2]->x, Vertices[2]->y, Vertices[2]->z);
-  printf("third face: %d %d %d \n", Triangles[2]->v1, Triangles[2]->v2, Triangles[2]->v3);
+  //printf("third face: %d %d %d \n", Triangles[2]->v1, Triangles[2]->v2, Triangles[2]->v3);
 }
 
 int main( int argc, char** argv ) {
+  
+
+  //make sure a file to read is specified
+  if (argc > 1) {
+    cout << "file " << argv[1] << endl;
+    //read-in the mesh file specified
+    ReadFile(argv[1]);
+    printFirstThree();
+    ColorVertices1(Triangles, Vertices);
+    printf("Done Coloring...\n"); 
+    Convert_to_Window(Vertices, 400, 400);
+
+    RasterizeTriangles(Triangles, Vertices, 400, 400);
+   }
   return 0;
 }
 
